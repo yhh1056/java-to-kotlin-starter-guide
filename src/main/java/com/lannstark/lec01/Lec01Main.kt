24a4b636b3af052697cd6e6e38dbbe1857a3b859
yhh1056 <hyeonho9995@gmail.com>
2023-01-10 19:40:23 +0900

    var, val, 초기화

---
+++ src/main/java/com/lannstark/lec01/Lec01Main.kt
@@ -0,0 +1,15 @@
+package com.lannstark.lec01
+
+fun main() {
+
+    var number1 = 10L
+    number1 = 5L
+
+    // value 불변... 짱이네...
+    val number2 = 10L
+    println(number2)
+
+    // new도 없어 하하
+    var hoho = Person("hoo")
+
+}
